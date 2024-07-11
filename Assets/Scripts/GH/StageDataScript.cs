e0ca18107f49677ffb9bc30b3ef1f92935810776
motical <motical9833@gmail.com>
2024-07-11 17:23:21 +0900

    이미지 추가 및 데이터 관련 내용 추가
    StageSelectUI 에 사용하는 StageImg추가 및 Data관련 내용 추가(회의 필요)

---
+++ Assets/Scripts/GH/StageDataScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageDataScript : MonoBehaviour
+{
+    public string stageLevel = "Level 1-1";
+    public int starCount = 0;
+    public int[] starCondition = new int[3];
+    public Sprite stageImg = null;
+
+    void Start()
+    {
+
+    }
+
+    void GetData()
+    {
+
+    }
+}
