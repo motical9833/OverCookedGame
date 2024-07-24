85e54528dd8c953b3b98a13f2ce373696ff6d8fb
motical <motical9833@gmail.com>
2024-07-24 20:17:14 +0900

    IngredientPool
    IngredientPool코드 작성

---
+++ Assets/Scripts/GH/IngredientObjectPoolScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientObjectPoolScript : MonoBehaviour
+{
+    private GameObject prefab;
+    private int poolSize = 20;
+    private Queue<GameObject> pool;
+    private string resourceName = "";
+
+    void Start()
+    {
+        pool = new Queue<GameObject>();
+
+        resourceName = gameObject.name + "/" + gameObject.name;
+
+        prefab = Resources.Load("3D/Food_Objects/" + resourceName) as GameObject;
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject obj = Instantiate(prefab);
+            obj.transform.parent = this.gameObject.transform;
+            obj.transform.localPosition = Vector3.zero;
+            obj.SetActive(false);
+            pool.Enqueue(obj);
+        }
+    }
+
+    public GameObject GetIngredientPoolObject()
+    {
+        if (pool.Count > 0)
+        {
+            GameObject obj = pool.Dequeue();
+            obj.SetActive(true);
+            return obj;
+        }
+        else
+        {
+            GameObject obj = Instantiate(prefab);
+            obj.SetActive(true);
+            return obj;
+        }
+    }
+
+    public void PushIngredientPool(GameObject obj)
+    {
+        obj.SetActive(false);
+        pool.Enqueue(obj);
+    }
+}
