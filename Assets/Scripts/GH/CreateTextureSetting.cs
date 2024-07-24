603b53128187bdb21b916b7b4a3e98d105749e8b
motical <motical9833@gmail.com>
2024-07-24 17:47:45 +0900

    Create 오브젝트 코드 작성
    IngredientsData의 값에 따라 Create 오브젝트의 Offset의 값을 변경

--- Assets/Scripts/GH/CreateTextureSetting.cs
+++ Assets/Scripts/GH/CreateTextureSetting.cs
@@ -1,28 +1,25 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreateScript : MonoBehaviour
+public class CreateTextureSetting : MonoBehaviour
 {
-    public Vector2 offset = Vector2.zero;
-    public Renderer skinnedMeshRenderer;
-    IngredientsData ingredientsData;
+
+    private Vector2 offset = Vector2.zero;
+    private Renderer skinnedMeshRenderer;
+    private IngredientsData ingredientsData;
     private MaterialPropertyBlock propertyBlock;
 
     void Start()
     {
         ingredientsData = GameObject.FindWithTag("StageManager").GetComponent<IngredientsData>();
         offset = ingredientsData.GetIngredientOffset(this.name);
-
         skinnedMeshRenderer = gameObject.GetComponent<Transform>().GetChild(2).GetComponent<Renderer>();
-
         propertyBlock = new MaterialPropertyBlock();
 
+
         skinnedMeshRenderer.GetPropertyBlock(propertyBlock);
-
         propertyBlock.SetVector("_MainTex_ST", new Vector4(0.5f, 0.5f, offset.x, offset.y));
-
         skinnedMeshRenderer.SetPropertyBlock(propertyBlock);
     }
 }
--- Assets/Scripts/GH/IngredientsData.cs
+++ Assets/Scripts/GH/IngredientsData.cs
@@ -1,16 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "IngredientsData", menuName = "ScriptableObjects/IngredientsData", order = 1)]
-public class IngredientsData : ScriptableObject
+public class IngredientsData : MonoBehaviour
 {
-    private Vector2 burgerBun = Vector2.zero;
-    private Vector2 onionOffset = new Vector2(0.25f, 0.5f);
+    public string csvFilePath = "Assets/Resources/IngredientData/IngredientData.csv";
+    private Dictionary<string, Vector2> ingredientOffsets;
 
-
-    public Vector2 GetOnionOffset()
+    private void Awake()
     {
-        return onionOffset;
+        LoadCSVData();
+        Vector2 onionOffset = GetIngredientOffset("Onion");
+        Debug.Log($"Onion Offset: {onionOffset}");
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void LoadCSVData()
+    {
+        ingredientOffsets = new Dictionary<string, Vector2>();
+
+
+        try
+        {
+            string[] lines = File.ReadAllLines(csvFilePath);
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] values = lines[i].Split(',');
+
+                if (values.Length == 3)
+                {
+                    string ingredient = values[0];
+                    float offsetX = float.Parse(values[1]);
+                    float offsetY = float.Parse(values[2]);
+                    ingredientOffsets.Add(ingredient, new Vector2(offsetX, offsetY));
+                }
+                else
+                {
+                    Debug.LogWarning("유효하지 않은 데이터 있음");
+                }
+
+            }
+        }
+        catch (IOException e)
+        {
+
+            Debug.LogError("csv파일을 읽을 수 없음");
+        }
+    }
+
+    public Vector2 GetIngredientOffset(string ingredient)
+    {
+        if(ingredientOffsets.TryGetValue(ingredient,out Vector2 offset))
+        {
+            return offset;
+        }
+        else
+        {
+            Debug.LogWarning("데이터를 찾을 수 없음");
+            return Vector2.zero;
+        }
     }
 }
