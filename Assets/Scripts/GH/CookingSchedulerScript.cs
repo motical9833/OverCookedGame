6766ac28295d3676b8895fa27103dabb5451e548
motical <motical9833@gmail.com>
2024-07-26 03:40:25 +0900

    요리 스케쥴러 스크립트 뼈대 작성
    CookingSchedulerScript 기반 코드 작성

--- Assets/Scripts/GH/CookingSchedulerScript.cs
+++ Assets/Scripts/GH/CookingSchedulerScript.cs
@@ -3,10 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Order
+{
+    public string orderName { get; set; }
+    public float timeLimit { get;set; }
+
+    public Order(string name ,float time)
+    {
+        orderName = name;
+        timeLimit = time;
+    }
+
+}
 
 public class CookingSchedulerScript : MonoBehaviour
 {
-    public List<Tuple<string, float>> order = new List<Tuple<string, float>>();
+    public List<Order> order = new List<Order>();
+
 
     void Start()
     {
@@ -15,6 +28,56 @@ public class CookingSchedulerScript : MonoBehaviour
 
     void Update()
     {
-        
+        if (order.Count > 0)
+        {
+            for (int i = order.Count-1; i >= 0; i--)
+            {
+                order[i].timeLimit -= Time.deltaTime;
+
+                if (order[i].timeLimit <= 0)
+                {
+                    order.RemoveAt(i);
+                }
+            }
+        }
+
+    }
+
+    public void SetOrder(Order orderData)
+    {
+        order.Add(orderData);
+    }
+
+    Order GetOrder(string str)
+    {
+        if(order.Count > 0)
+        {
+            for (int i = 0; i < order.Count; i++)
+            {
+                if (order[i].orderName == str)
+                {
+                    return order[i];
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public void MakeDish(string dishName)
+    {
+        if (order.Count > 0)
+        {
+            for (int i = 0; i < order.Count; i++)
+            {
+                if (order[i].orderName == dishName)
+                {
+                    //Á¡¼ö È¹µæ ·ÎÁ÷ ÀÛ¼º
+                    break;
+                }
+            }
+        }
+
+        return;
     }
 }
