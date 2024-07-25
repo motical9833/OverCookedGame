679f3014b9023f182eb4e2b897383365654db954
motical <motical9833@gmail.com>
2024-07-25 17:41:45 +0900

    GetPull

---
+++ Assets/Scripts/GH/CookingSchedulerScript.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CookingSchedulerScript : MonoBehaviour
+{
+    public List<Tuple<string, float>> order = new List<Tuple<string, float>>();
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
