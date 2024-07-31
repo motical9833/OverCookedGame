e53054a67645063e126fe106a9983b1f00576db1
motical <motical9833@gmail.com>
2024-08-01 01:22:55 +0900

    OrdereUIScript 코드 수정
    시간에 흐름에 따라 타이머BarUI의 컬러 색상이 변경되도록 구현

--- Assets/Scripts/GH/OrderUIScript.cs
+++ Assets/Scripts/GH/OrderUIScript.cs
@@ -6,25 +6,36 @@ using UnityEngine.UI;
 public class OrderUIScript : MonoBehaviour
 {
     Slider slider;
-    public float currentTimer = 80.0f;
+    public Image fill;
 
-    private float initialTimer;
+    Color greenColor = Color.green;
+    Color redColor = Color.red;
+    private float initialTimer = 80.0f;
+
+    float currentTimer;
 
     void Start()
     {
-        initialTimer = currentTimer;
+        currentTimer = initialTimer;
         slider = this.gameObject.GetComponent<Transform>().GetChild(2).GetComponent<Slider>();
+        fill = slider.GetComponent<Transform>().GetChild(1).GetChild(0).GetComponent<Image>();
+
+        fill.color = greenColor;
     }
 
     void Update()
     {
         currentTimer -= Time.deltaTime;
 
-        float ratio = currentTimer / initialTimer;
+        float ratio = Mathf.Clamp01(currentTimer / initialTimer);
 
         slider.value = ratio;
 
-        if(currentTimer < 0)
+        float t = Mathf.PingPong(currentTimer / initialTimer, 1.0f);
+
+        fill.color = Color.Lerp(redColor, greenColor, t);
+
+        if (currentTimer <= 0)
         {
             ResetTimer();
         }
@@ -39,5 +50,6 @@ public class OrderUIScript : MonoBehaviour
     {
         currentTimer = 80.0f;
         slider.value = 1;
+        fill.color = greenColor;
     }
 }
