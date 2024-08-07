cd8efff9dc0c97f7341f510cef9dd77d5c998479
KailCrane <64236907+KailCrane@users.noreply.github.com>
2024-08-07 17:29:19 +0900

    양파 애니메이션 및 토마토 애니메이션 추가

--- Assets/Scripts/Yj/Item/IngredientScript.cs
+++ Assets/Scripts/Yj/Item/IngredientScript.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class IngredientScript : GrabAbleObjScript
 {
     bool isChopped = false;
-
     float cutting_time;
-
+    Animator animator;
 
     public override void Initialize()
     {
         base.Initialize();
+        animator = GetComponent<Animator>();
     }
-   
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Chop();
+        }
+    }
+
     public void Chop()
     {
+
         Debug.Log("음식물 썰리는 중");
     }
 
