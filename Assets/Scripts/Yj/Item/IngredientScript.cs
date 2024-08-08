7e12054ad942740ab4a3750d62e2bb3f4afd0bd4
KailCrane <64236907+KailCrane@users.noreply.github.com>
2024-08-08 15:01:50 +0900

    애니메이션 컨트롤러 위치 변경
    동적 애니메이션 컨트롤러 할당을 위한 위치 변경 및 스크립트 변경

--- Assets/Scripts/Yj/Item/IngredientScript.cs
+++ Assets/Scripts/Yj/Item/IngredientScript.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class IngredientScript : GrabAbleObjScript
 {
     bool isChopped = false;
     float cutting_time;
+
     Animator animator;
+    public string controllerPath = "AnimatorController";  // Resources 폴더 안의 경로 (확장자 제외)
 
     public override void Initialize()
     {
         base.Initialize();
-        animator = GetComponent<Animator>();
+        animator = transform.AddComponent<Animator>();
+        string name = gameObject.name;
+        string path = name.Replace("(Clone)", "");
+        RuntimeAnimatorController controller = Resources.Load<RuntimeAnimatorController>(controllerPath + "/" + path);
+        animator.runtimeAnimatorController = controller;
     }
 
     private void Update()
@@ -21,6 +28,21 @@ public class IngredientScript : GrabAbleObjScript
             Chop();
         }
     }
+
+    public void Gather()
+    {
+        if (!isChopped)
+            return;
+
+
+
+    }
+
+    public void Drop()
+    {
+
+    }
+
 
     public void Chop()
     {
