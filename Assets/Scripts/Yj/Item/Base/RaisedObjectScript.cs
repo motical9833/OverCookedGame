22ea292dab0ae62c66e2d4abb76c87b2f7161d9f
KailCrane <64236907+KailCrane@users.noreply.github.com>
2024-08-12 15:42:01 +0900

    Chopping 행동 수정
    chopping 중 board를 떠나갔을 때 Idlle 전환과 board의 chop -1 chopping이 끝났을때 플레이어 애니메이션 Idle 전환

--- Assets/Scripts/Yj/Item/Base/RaisedObjectScript.cs
+++ Assets/Scripts/Yj/Item/Base/RaisedObjectScript.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class RaisedObjectScript : MonoBehaviour
+public class RaisedObjectScript : NetworkBehaviour
 {
     public GameObject raisedObj;
     public void RaisObject(GameObject _raisedObj)
--- Assets/Scripts/Yj/Item/IngredientScript.cs
+++ Assets/Scripts/Yj/Item/IngredientScript.cs
@@ -6,48 +6,63 @@ using UnityEngine;
 public class IngredientScript : GrabAbleObjScript
 {
     bool isChopped = false;
+    bool isChoppingProgress = false;
     float cutting_time;
 
     Animator animator;
     public string controllerPath = "AnimatorController";  // Resources 폴더 안의 경로 (확장자 제외)
+    float sliceGuage;
+
+    private GameObject wholeObj;
+    private GameObject sliceObj;
+    bool isFirstSlice = false;
 
     public override void Initialize()
     {
         base.Initialize();
+        SetAnimator();
+        wholeObj = transform.GetChild(0).Find("Onion_Mesh").transform.Find("Onion_Whole").gameObject;
+        sliceObj = transform.GetChild(0).Find("Onion_Mesh").transform.Find("Onion_Sliced").gameObject;
+        sliceObj.SetActive(false);
+    }
+  
+    private void SetAnimator()
+    {
         animator = transform.AddComponent<Animator>();
         string name = gameObject.name;
         string path = name.Replace("(Clone)", "");
         RuntimeAnimatorController controller = Resources.Load<RuntimeAnimatorController>(controllerPath + "/" + path);
         animator.runtimeAnimatorController = controller;
     }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Chop();
-        }
-    }
-
     public void Gather()
     {
         if (!isChopped)
             return;
-
-
-
+        animator.SetTrigger("Gather");
     }
 
     public void Drop()
     {
-
+        animator.SetTrigger("Drop");
     }
 
-
-    public void Chop()
+    public void Chopping()
     {
-
-        Debug.Log("음식물 썰리는 중");
+        if (!isChopped)
+        {
+            sliceGuage += Time.deltaTime * 33;
+            animator.SetFloat("Slice", sliceGuage);
+            if (!isFirstSlice && sliceGuage <= 33)
+            {
+                isFirstSlice = true;
+                wholeObj.SetActive(false);
+                sliceObj.SetActive(true);
+            }
+            if (sliceGuage >= 100)
+            {
+                isChopped = true;
+            }
+        }
     }
 
     public bool GetIsChopped()
