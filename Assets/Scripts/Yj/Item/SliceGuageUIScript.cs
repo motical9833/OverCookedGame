6dc087c9bc9bbaeec50b8ec97fb8bd112920a72b
KailCrane <64236907+KailCrane@users.noreply.github.com>
2024-08-16 13:43:42 +0900

    Slice UI의 프레임

---
+++ Assets/Scripts/Yj/Item/SliceGuageUIScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliceGuageUIScript : MonoBehaviour
+{
+    Canvas canvas;
+    Slider sliderUi;
+
+    private void Update()
+    {
+
+    }
+    
+    void RookCamera()
+    {
+
+    }
+
+    void ShowSliceUI()
+    {
+
+    }
+
+    void DisableSliceUI()
+    {
+
+    }
+
+    // 카메라에 맞게 각도 맞춰주기..
+    // slice가 진행 중일때만 Ui를 보여주기
+
+}
