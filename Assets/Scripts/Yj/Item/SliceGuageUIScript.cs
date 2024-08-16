2dc4a67f858a38aa42c9ce160b4aa1a9a7fff7e6
KailCrane <64236907+KailCrane@users.noreply.github.com>
2024-08-16 14:05:36 +0900

    SliceUI의 초안

--- Assets/Scripts/Yj/Item/SliceGuageUIScript.cs
+++ Assets/Scripts/Yj/Item/SliceGuageUIScript.cs
@@ -5,27 +5,38 @@ using UnityEngine.UI;
 
 public class SliceGuageUIScript : MonoBehaviour
 {
-    Canvas canvas;
-    Slider sliderUi;
+    private Transform targetCamTr;
 
-    private void Update()
+    private Canvas canvas;
+    private Slider sliderUi;
+
+
+    private void Start()
     {
-
+        targetCamTr = Camera.main.transform;
+        sliderUi.minValue = 0.0f;
+        sliderUi.maxValue = 100.0f;
+        sliderUi.value = 0.0f;
     }
-    
+
     void RookCamera()
     {
-
+        //x축만 바뀌어야함
+        Vector3 directionToCamera = targetCamTr.position - transform.position;
+        directionToCamera.y = 0;
+        Quaternion targetRotation = Quaternion.LookRotation(directionToCamera);
+        transform.rotation = targetRotation;
     }
 
-    void ShowSliceUI()
+    void ShowSliceUI(float guage)
     {
-
+        sliderUi.enabled = true;
+        sliderUi.value = guage;
     }
 
     void DisableSliceUI()
     {
-
+        sliderUi.enabled = false;
     }
 
     // 카메라에 맞게 각도 맞춰주기..
