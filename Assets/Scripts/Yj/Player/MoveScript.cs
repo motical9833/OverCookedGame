8416ecd7d54eef2e03985e19499d77b8d166cfcd
KailCrane <64236907+KailCrane@users.noreply.github.com>
2024-09-10 13:51:16 +0900

    대시 추가

--- Assets/Scripts/Yj/Player/MoveScript.cs
+++ Assets/Scripts/Yj/Player/MoveScript.cs
@@ -9,18 +9,41 @@ public class MoveScript : MonoBehaviour
     float moveSpeed;
     float rotSpeed;
 
+    float dashAccMaxSpeed = 0.07f;
+    float dashAccSpeed = 0.0f;
+
+    bool isDashing;
+    bool isDashCool;
+    bool isReachMaxSpeed = false;
+
+
     public void SetInitial(GameObject _model, float _moveSpeed, float _rotSpeed)
     {
         model = _model;
         moveSpeed = _moveSpeed;
         rotSpeed = _rotSpeed;
     }
+    private void Update()
+    {
+       
+    }
+
+    public IEnumerator Dash()
+    {
+        if(isDashing)
+        {
+            yield return null;
+        }
+        dashAccSpeed = dashAccMaxSpeed;
+        yield return new WaitForSeconds(0.5f);
+        dashAccSpeed = 0.0f;
+    }
 
  
     public void Move(float h, float v)
     {
-        transform.Translate(h * moveSpeed * Time.deltaTime, 0.0f,
-                         v * moveSpeed * Time.deltaTime);
+        transform.Translate(h * (moveSpeed + dashAccSpeed) * Time.deltaTime, 0.0f,
+                         v * (moveSpeed + dashAccSpeed) * Time.deltaTime);
 
         Vector3 moveDirection = new Vector3(h, 0f, v).normalized;
 
